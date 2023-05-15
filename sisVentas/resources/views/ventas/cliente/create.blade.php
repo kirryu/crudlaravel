5d31bef9182adefcca1cf266ee64512cc841344d
kirryu <zerokirryu06@gmail.com>
2023-05-15 01:20:36 -0500

    primer commit

---
+++ sisVentas/resources/views/ventas/cliente/create.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.admin')
+@section('contenido')
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <h3>Nueva Cliente</h3>
+            @if(count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+        </div>
+    </div>    
+            {!!Form::open(array('url'=>'ventas/cliente','method'=>'POST','autocomplete'=>'off'))!!}
+            {{Form::token()}}
+    <div class="row">
+        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12" >
+            <div class="form-group">
+                <label for="nombre">Nombre</label>
+                <input type="text" name="nombre"  require value="{{ old('nombre') }}" class="form-control" placeholder="Nombre...">
+            </div>
+        </div>
+        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12" >
+            <div class="form-group">
+                <label for="nombre">Direccion</label>
+                <input type="text" name="direccion"  class="form-control" placeholder="Direccion...">
+            </div>
+        </div>
+        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12" >
+            <div class="form-group">
+                <label >Tipo de Documento</label>
+                <select name="tipo_documento" class="form-control">
+                        <option value="DNI">DNI</option>
+                        <option value="RUC">RUC</option>
+                        <option value="PAS">Pasaporte</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12" >
+            <div class="form-group">
+                <label for="num_documento">Numero de Documento</label>
+                <input type="text" name="num_documento" value="{{old('num_documento')}}" class="form-control" placeholder="Numero de Documento..." >
+            </div>
+        </div>
+        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12" >
+            <div class="form-group">
+                <label for="telefono" >Telefono</label>
+                <input type="text" name="telefono" value="{{ old('telefono') }}" class="form-control" placeholder="Telefono...">
+            </div>
+        </div>
+        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12" >
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email"  value="{{ old('email') }}" class="form-control" placeholder="Correo Eletronico..." >
+            </div>
+        </div>
+
+        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+            <div class="form-group">
+                <button class="btn btn-primary" type="submit">Guardar</button>
+                <button type="reset" class="btn btn-danger">Cancelar</button>
+            </div>
+        </div>
+    </div>
+            
+            
+            {!!Form::close()!!}
+@endsection 
